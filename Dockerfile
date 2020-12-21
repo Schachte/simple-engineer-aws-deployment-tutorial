f161b3b4194f6bf73f509a0f2feebcfa067b3030
Ryan Schachte <code@ryan-schachte.com>
2020-12-20 21:43:50 -0700

    init commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:alpine
+WORKDIR '/app'
+COPY package.json ./
+RUN npm install
+COPY . . 
+RUN npm run build
+
+FROM nginx
+COPY --from=0 /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          test updates
         </p>
         <a
           className="App-link"
